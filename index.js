f6531f3a79f36bdbe2280a4e1a487b3dc6b83a11
unknown <hoobaloob99@gmail.com>
2021-02-11 16:56:57 -0700

    uploaded from with cmd

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ var shell = require("shelljs")
 var {exec} = require("child_process")
 
 
-var addFunc = async () => {
-    exec('git add .');
-}
+// var addFunc = async () => {
+//     exec('git add .');
+// }
 
-var commitFunc = async () => {
-    await exec('git commit -m "submitted with child process 439');
-}
+// var commitFunc = async () => {
+//     await exec('git commit -m "submitted with child process 439');
+// }
 
-var pushFunc = async () => {
-    await exec('git push');
-}
+// var pushFunc = async () => {
+//     await exec('git push');
+// }
 
-var cmdStack = async () =>{
-    await exec('git push')
-}
+// var cmdStack = async () =>{
+//     await exec('git push')
+// }
+
+
 
 const Timer = async () => {
     let seconds = 0
     let interval = 0 
-    // setTimeout(()=>{
-        //     cmd.exec("git add .")
-        //     cmd.exec('git commit -m "uploaded from with cmd"')
-        //     cmd.exec('git push')
-        // },10)
-        await addFunc()
-        await commitFunc()
-        await pushFunc()
+    setTimeout(()=>{
+            shell.exec("git add .")
+            shell.exec('git commit -m "uploaded from with cmd"')
+            shell.exec('git push')
+        },10)
+        // await addFunc()
+        // await commitFunc()
+        // await pushFunc()
     }
 
     Timer()
