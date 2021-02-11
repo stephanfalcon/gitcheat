0dad426bbc729daa032f9ad958b2601b4dcf6ac8
unknown <hoobaloob99@gmail.com>
2021-02-11 16:12:46 -0700

    uploaded from index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var shell = require("shelljs")
 const Timer = () => {
     let seconds = 0
     let interval = 0 
-
+    setInterval(()=>{
         shell.exec("git add .")
         shell.exec('git commit -m "uploaded from index.js"')
         shell.exec('git push')
+    },10000)
 }
 
 Timer()
