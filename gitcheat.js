626463f0187a1584d418bfaed0b9606f7bb3c152
unknown <hoobaloob99@gmail.com>
2021-02-11 09:40:55 -0700

    submitted with gitcheat

---
+++ gitcheat.js
@@ -0,0 +1,37 @@
+const {exec} = require("child_process")
+
+exec("git add .",(error, stdout,stderr)=>{
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
+
+exec('git commit -m "submitted with gitcheat"',(error, stdout,stderr)=>{
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
+
+exec("git git push",(error, stdout,stderr)=>{
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
