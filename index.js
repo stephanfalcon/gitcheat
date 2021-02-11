601ce76938d8dc140917b13d419931b77a512450
unknown <hoobaloob99@gmail.com>
2021-02-11 16:36:41 -0700

    submitted with child process

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 var shell = require("shelljs")
+var {exec} = require("child_process")
+
+
+var addFunc = () => {
+    exec('git add .', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
+
+var commitFunc = () => {
+    exec('git commit -m "submitted with child process', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
+
+var pushFunc = () => {
+    exec('git push', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+}
 
 const Timer = () => {
     let seconds = 0
     let interval = 0 
-    setInterval(()=>{
-        shell.exec("git add .")
-        shell.exec('git commit -m "uploaded from index.js"')
-        shell.exec('git push')
-    },10000)
-}
+    // setTimeout(()=>{
+        //     cmd.exec("git add .")
+        //     cmd.exec('git commit -m "uploaded from with cmd"')
+        //     cmd.exec('git push')
+        // },10)
+        addFunc()
+        commitFunc()
+        pushFunc()
+    }
 
-Timer()
+    Timer()
