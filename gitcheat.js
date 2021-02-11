608a7f7e4cfea86000558b13a25e335709df5430
unknown <hoobaloob99@gmail.com>
2021-02-11 12:56:22 -0700

    gitcheat

--- gitcheat.js
+++ gitcheat.js
@@ -1,124 +1,51 @@
 const {exec} = require("child_process")
 
-// const fs = require('fs')
-
-// const path = './.git/index.lock'
-
-var d = new Date();
-var n = d.toString();
-
-
-// const addFunc = () => {
-//     return new Promise((resolve,reject)=>{
-//         exec("git add .",(error, stdout,stderr)=>{
-//             if (error) {
-//                 console.log(`add error: ${error.message}`);
-//                 reject(error.message)
-//             }
-//             if (stderr) {
-//                 console.log(`add stderr: ${stderr}`);
-//             }
-//             setTimeout(()=>{
-//                 console.log(`add stdout: ${stdout}`);
-//                 resolve()
-//             },250)
-
-//         })
-//     })
-// }
-
-// const commitFunc = () => {
-
-//     return new Promise((resolve,reject)=>{
-//                 exec(`git commit -m "submitted with gitcheat; interval: ${n}"`,(error, stdout,stderr)=>{
-//             if (error) {
-//                 try {
-//                     fs.unlinkSync(path)
-
-//                 } catch(err) {
-//                 }
-//             }
-//             if (stderr) {
-//                 console.log(`commit stderr: ${stderr}`);
-//             }
-//             setTimeout(()=>{
-//                 console.log(`commit stdout: ${stdout}`);
-//                 resolve()
-//             },250)
-//         }) 
-//     })
-// }
-
-// const pushFunc = () => {
-//     try {
-//         fs.unlinkSync(path)
-
-//       } catch(err) {
-//       }
-//     return new Promise((resolve,reject)=>{
-//         exec(`git push`,(error, stdout,stderr)=>{
-//             if (error) {
-//                 console.log(`push error: ${error.message}`);
-//                 return reject(error)
-//             }
-//             if (stderr) {
-//                 console.log(`push stderr: ${stderr}`);
-//                 return resolve()
-//             }
-//             setTimeout(()=>{
-//                 console.log(`push stdout: ${stdout}`);
-//                 resolve()
-//             },250)
-//         })
-//     })
-// }
-
-// addFunc()
-// .then(commitFunc()
-// .then(pushFunc()
-// .catch()))
-
 const addFunc = () => {
-    return new Promise((resolve,reject)=>{
-        exec("git add .",(error, stdout,stderr)=>{
-            if (error) {
-                console.log(`add error: ${error.message}`);
-                reject(error.message)
-            }
-            if (stderr) {
-                console.log(`add stderr: ${stderr}`);
-            }
-            setTimeout(()=>{
-                console.log(`add stdout: ${stdout}`);
-                resolve()
-            },250)
+    exec(`git add .`,(error, stdout,stderr)=>{
+        if (error) {
+            console.log(`error: ${error.message}`);
 
-        })
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+
+        }
+        console.log(`stdout: ${stdout}`);
+
     })
 }
 
 const commitFunc = () => {
+    exec(`git commit`,(error, stdout,stderr)=>{
+        if (error) {
+            console.log(`error: ${error.message}`);
 
-    return new Promise((resolve,reject)=>{
-                exec(`git commit -m "submitted with gitcheat; interval: ${n}"`,(error, stdout,stderr)=>{
-            if (error) {
-                try {
-                    fs.unlinkSync(path)
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
 
-                } catch(err) {
-                }
-            }
-            if (stderr) {
-                console.log(`commit stderr: ${stderr}`);
-                exec('rm -f ./.git/index.lock',(error, stdout,stderr)=>{})
-            }
-            setTimeout(()=>{
-                console.log(`commit stdout: ${stdout}`);
-                resolve()
-            },250)
-        }) 
+        }
+        console.log(`stdout: ${stdout}`);
+
     })
 }
 
+const pushFunc = () => {
+
+        exec(`git push`,(error, stdout,stderr)=>{
+            if (error) {
+                console.log(`error: ${error.message}`);
+
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+
+            }
+            console.log(`stdout: ${stdout}`);
+ 
+        })
+    }
+
 addFunc()
-.then(commitFunc())
+commitFunc()
+pushFunc()
